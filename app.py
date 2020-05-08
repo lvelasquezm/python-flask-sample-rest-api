231cd4522b6bb8a835e25c5fc3c7d93d1b340b11
Lawrence Velasquez <law.vzmk@gmail.com>
2020-05-08 17:59:07 -0600

    Add flask-jwt-extended callbacks and responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -28,11 +28,53 @@ def create_tables():
 
 jwt = JWTManager(app)
 
+# Add claims to all requests
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
+# Access token has expired
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'message': 'The access token has expired.',
+        'error': 'access_token_expired',
+    }), 401
+
+# Access token is invalid (not a valid access token)
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'message': 'Invalid access token.',
+        'error': 'invalid_access_token',
+    }), 401
+
+# Access token was not sent in the request
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'message': 'Request does not contain an access token.',
+        'error': 'access_token_required',
+    }), 401
+
+# Access token was sent in the request and is not a fresh one
+# but the endpoint requires it to be a fresh one
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback(error):
+    return jsonify({
+        'message': 'The access token is not fresh.',
+        'error': 'fresh_access_token_required',
+    }), 401
+
+# Revoke a token
+@jwt.revoked_token_loader
+def revoke_token_callback(error):
+    return jsonify({
+        'message': 'The access token has been revoked.',
+        'error': 'access_token_required',
+    }), 401
 
 api.add_resource(User, '/user/<int:name>')
 api.add_resource(UserLogin, '/login')
